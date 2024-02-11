bbdaac98bdd2201d8cafff1d1f322bdd8d714871
KDkirtiman <kirtimansingh205@gmail.com>
2024-02-11 21:30:11 +0530

    first commit

---
+++ src/main/java/com/lbc/LocalBusinessConnectApplication.java
@@ -0,0 +1,13 @@
+package com.lbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBusinessConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalBusinessConnectApplication.class, args);
+	}
+
+}
